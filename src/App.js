010da436ecd4de57b29d65c9081fcaf8555c228d
Stu Bolderson <stu.bolderson@aol.com>
2021-10-28 16:54:50 +0200

    About us section componenet created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import { AboutUs } from "./pages/AboutUs";
+
 export const App = () => {
   return (
     <div className="App">
-      <h1>Hello Stu</h1>
+      <AboutUs />
     </div>
   );
 };
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { AboutSection } from "../components/AboutSection";
+
+export const AboutUs = () => <AboutSection />;
