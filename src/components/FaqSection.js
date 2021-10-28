3df44c0e024f7210c24bd574e423f96167954f18
Stu Bolderson <stu.bolderson@aol.com>
2021-10-28 18:32:13 +0200

    Faq section added

---
+++ src/components/FaqSection.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      {/* Question */}
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse totam
+            libero sed dolores assumenda nemo id voluptatibus dolore odio iste.
+          </p>
+        </div>
+      </div>
+      {/* Question */}
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse totam
+            libero sed dolores assumenda nemo id voluptatibus dolore odio iste.
+          </p>
+        </div>
+      </div>
+      {/* Question */}
+      <div className="question">
+        <h4>Different payment methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse totam
+            libero sed dolores assumenda nemo id voluptatibus dolore odio iste.
+          </p>
+        </div>
+      </div>
+      {/* Question */}
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse totam
+            libero sed dolores assumenda nemo id voluptatibus dolore odio iste.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { AboutSection } from "../components/AboutSection";
+import { FaqSection } from "../components/FaqSection";
 import { ServicesSection } from "../components/ServicesSection";
 
 export const AboutUs = () => (
   <>
     <AboutSection />
     <ServicesSection />
+    <FaqSection />
   </>
 );
