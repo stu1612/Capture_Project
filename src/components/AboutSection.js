6c3aa2596b257d3e138e2e2c57036d32b86901c0
Stu Bolderson <stu.bolderson@aol.com>
2021-10-31 08:05:35 +0100

    our work animations working correctly

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,33 +6,37 @@ import {
   StyledImage,
   StyledHide,
 } from "../styles";
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation/animation";
+import { Wave } from "./Wave";
 
 export const AboutSection = () => {
   return (
     <StyledAbout>
       <StyledDescription>
-        <div className="title">
+        <motion.div>
           <StyledHide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </StyledHide>
           <StyledHide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </StyledHide>
           <StyledHide>
-            <h2>true</h2>
+            <motion.h2 variants={titleAnim}>true</motion.h2>
           </StyledHide>
-        </div>
-        <p>
+        </motion.div>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </StyledDescription>
       <StyledImage>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </StyledImage>
+      <Wave />
     </StyledAbout>
   );
 };
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -3,7 +3,14 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 // Animations
 import { motion } from "framer-motion";
-import { pageAnim } from "../animation/animation";
+import {
+  pageAnim,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../animation/animation";
 
 import { MovieState } from "../Data/movies";
 
@@ -15,25 +22,38 @@ export const OurWork = () => {
   }, []);
 
   return (
-    <>
-      <StyledWork
-        style={{ background: "#fff" }}
-        exit="exit"
-        variants={pageAnim}
-        initial="hidden"
-        animate="show"
-      >
-        {data.map((movie) => (
-          <StyledMovie key={movie.id}>
-            <h2>{movie.title}</h2>
-            <div className="line"></div>
-            <Link to={movie.url}>
-              <img src={movie.mainImg} alt={movie.title} />
-            </Link>
-          </StyledMovie>
-        ))}
-      </StyledWork>
-    </>
+    <StyledWork
+      style={{ background: "#fff" }}
+      exit="exit"
+      variants={pageAnim}
+      initial="hidden"
+      animate="show"
+    >
+      {/* frame animations */}
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
+      {/* frame animations */}
+
+      {data.map((movie) => (
+        <StyledMovie key={movie.id}>
+          <motion.h2 variants={fade}>{movie.title}</motion.h2>
+          <motion.div variants={lineAnim} className="line"></motion.div>
+          <Link to={movie.url}>
+            <Hide>
+              <motion.img
+                variants={photoAnim}
+                src={movie.mainImg}
+                alt={movie.title}
+              />
+            </Hide>
+          </Link>
+        </StyledMovie>
+      ))}
+    </StyledWork>
   );
 };
 
@@ -45,11 +65,11 @@ const StyledWork = styled(motion.div)`
     padding: 1rem 0rem;
   }
 `;
-const StyledMovie = styled.div`
+const StyledMovie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background: #cccccc;
+    background: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -57,4 +77,31 @@ const StyledMovie = styled.div`
     height: 70vh;
     object-fit: cover;
   }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+//Frame animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
